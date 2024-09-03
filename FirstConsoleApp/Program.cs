aca4c5ee81d953f144cb4d7119ac39f4c456ce5a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-03 11:40:48 +0300

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// коментарі
+// Ctrl + F5 - запуск програми
+
+// робота з консолью
+Console.WriteLine("Hello C# again!");
+
+// cw + TAB = Console.WriteLine();
+Console.WriteLine("This is text line...");
+
+// WriteLine - ставить новий рядок вкінці
+// Write - не ставить новий рядок після тексту
+
+Console.Write("Red");
+Console.Write("Green");
+Console.Write("Blue");
+
+// спец символи: \n \t \' \"
+
+Console.Write("\nNew Line\tTab\t\tBye!!!\n\n");
+
+// отримати значення від користувача
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Скільки ти маєш грошей? ");
+int money = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Ти має {money / 41} долірів)");
