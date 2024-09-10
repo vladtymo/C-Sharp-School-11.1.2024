0e31d6777962e4b65f334d0336ccdd01e236233e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-10 11:52:00 +0300

    Add examples: circle area and miles

---
+++ 02_practical/Program.cs
@@ -0,0 +1,24 @@
+﻿// Завдання: написати програму, яка запитує в користувача к-сть мить та переводить в км
+
+// 1. запитати значення
+// 2. обрахунок значення
+// 3. відобразити результат
+
+Console.WriteLine("----------------- Convert Miles to Kilometers -----------------");
+Console.Write("Enter miles: ");
+
+// тип.Parse(рядок) - перетворення рядка до певного типу
+float miles = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Result: {miles * 1.60934} km");
+
+// Завдання: написати програму, яка запитує радіус кола та визначає його площу
+
+Console.WriteLine("----------------- Circle Area by Radius -----------------");
+Console.Write("Enter radius (cm): ");
+
+// тип.Parse(рядок) - перетворення рядка до певного типу
+float r = float.Parse(Console.ReadLine());
+double area = Math.PI * Math.Pow(r, 2);
+
+Console.WriteLine($"Result: {area} cm^2");
