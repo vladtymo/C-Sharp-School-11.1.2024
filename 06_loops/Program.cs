1f55d53a0e4f956f94ef8b1a81fe520be2d0ca64
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-11 10:52:54 +0300

    While loop example

---
+++ 06_loops/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Enter product quantity: ");
+int quantity = int.Parse(Console.ReadLine());
+
+//if (quantity < 0)
+//{
+//    Console.WriteLine("Cannot be negative!");
+//    Console.Write("Enter again: ");
+//    quantity = int.Parse(Console.ReadLine());
+//}
+
+while (quantity < 0)
+{
+    Console.WriteLine("Cannot be negative!");
+    Console.Write("Enter again: ");
+    quantity = int.Parse(Console.ReadLine());
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Great!");
+Console.ResetColor();
+
+// Завдання: Показати на екран 10 разів слово "Привіт"
+
+Console.Write("Enter count: ");
+int n = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+while (count < n)
+{
+    Console.WriteLine($"{count} - Hello!");
+    ++count;
+}
