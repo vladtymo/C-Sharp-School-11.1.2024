f4d4d0f66c97e4025558a1405b4622d918095bf2
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-22 11:55:41 +0300

    Complete puzzle game

--- 08_game_puzzle/Program.cs
+++ 08_game_puzzle/Program.cs
@@ -1,14 +1,15 @@
 ﻿// Створення гри пятнашки
+const int size = 4;
 
 // створення масиву комірок
 int zeroY = 2;
 int zeroX = 1;
 
-int[,] puzzle = new int[4, 4] {
-    { 4, 10, 9, 3 },
-    { 8, 2, 1, 5 },
-    { 11, 0, 14, 6 },
-    { 7, 15, 12, 13 }
+int[,] puzzle = new int[size, size] {
+    { 4, 2, 9, 3 },
+    { 8, 10, 7, 5 },
+    { 11, 16, 14, 6 },
+    { 1, 15, 12, 13 }
 };
 
 PrintPuzzle();
@@ -20,18 +21,30 @@ while (true)
     switch (key)
     {
         case ConsoleKey.LeftArrow:
+            if (zeroX == size - 1) break;
             // move left
             // swap 0 <> right cell
             Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY, zeroX + 1]);
             ++zeroX;
             break;
         case ConsoleKey.RightArrow:
+            // якщо порожня комірка на 1-й колонці
+            if (zeroX == 0) break;
             // move right
             Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY, zeroX - 1]);
             --zeroX;
             break;
         case ConsoleKey.UpArrow:
+            if (zeroY == size - 1) break;
             // move up
+            Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY + 1, zeroX]);
+            ++zeroY;
+            break;
+        case ConsoleKey.DownArrow:
+            if (zeroY == 0) break;
+            // move up
+            Swap(ref puzzle[zeroY, zeroX], ref puzzle[zeroY - 1, zeroX]);
+            --zeroY;
             break;
     }
 
@@ -48,14 +61,19 @@ void Swap(ref int a, ref int b)
 
 void PrintPuzzle()
 {
-    for (int r = 0; r < 4; r++)
+    for (int r = 0; r < size; r++)
     {
-        for (int c = 0; c < 4; c++)
+        for (int c = 0; c < size; c++)
         {
-            if (puzzle[r, c] == 0)
+            if (puzzle[r, c] == r * size + c + 1)
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            if (puzzle[r, c] == 16)
                 Console.Write("[  ] ");
             else
                 Console.Write($"[{puzzle[r, c], 2}] ");
+
+            Console.ResetColor();
         }
         Console.WriteLine();
     }
