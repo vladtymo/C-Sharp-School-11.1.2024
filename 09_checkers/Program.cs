c6ff61643776cda68d8e215ce34ea8886d686313
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-05 12:25:17 +0200

    Create board

---
+++ 09_checkers/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    const int size = 8;
+    static int[,] board = new int[size, size];
+    
+    public static void Main(string[] args)
+    {
+        PrintBoard();
+    }
+
+    static void PrintBoard()
+    {
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size / 2; x++)
+            {
+                if (y % 2 == 0)
+                    Console.Write("\u2591\u2591  ");
+                else
+                    Console.Write("  \u2591\u2591");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
