73c6131ed22c0b909286a15dfaefe22c7f32f134
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-08 11:04:52 +0200

    Create array

--- 09_checkers/Program.cs
+++ 09_checkers/Program.cs
@@ -1,13 +1,42 @@
 ﻿internal class Program
 {
     const int size = 8;
-    static int[,] board = new int[size, size];
+    const int white = 1;
+    const int black = 2;
+
+    private static int[,] board = new int[size, size]
+    {
+        { 0, 1, 0, 1, 0, 1, 0, 1 },
+        { 1, 0, 1, 0, 1, 0, 1, 0 },
+        { 0, 1, 0, 1, 0, 1, 0, 1 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0, 0, 0, 0 },
+        { 2, 0, 2, 0, 2, 0, 2, 0 },
+        { 0, 2, 0, 2, 0, 2, 0, 2 },
+        { 2, 0, 2, 0, 2, 0, 2, 0 }
+    };
     
     public static void Main(string[] args)
     {
+        Console.Clear();
+        Console.CursorVisible = false;
+        
         PrintBoard();
+
+        PrintFigures();
+
+        Console.ReadKey();
     }
 
+    static void PrintFigures()
+    {
+        Console.SetCursorPosition(1, 0);
+        Console.Write("\u25C9");
+        
+        Console.SetCursorPosition(4, 0);
+        Console.Write("\u25CC");
+    }
+    
     static void PrintBoard()
     {
         for (int y = 0; y < size; y++)
@@ -15,9 +44,9 @@
             for (int x = 0; x < size / 2; x++)
             {
                 if (y % 2 == 0)
-                    Console.Write("\u2591\u2591  ");
+                    Console.Write("\u2591\u2591\u2591\u2592\u2592\u2592");
                 else
-                    Console.Write("  \u2591\u2591");
+                    Console.Write("\u2592\u2592\u2592\u2591\u2591\u2591");
             }
 
             Console.WriteLine();
