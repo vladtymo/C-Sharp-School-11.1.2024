30fafa316458cfbdaceaab4d31b767cb1bfe6c17
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-19 11:35:27 +0200

    Methods

---
+++ 10_methods/Program.cs
@@ -0,0 +1,28 @@
+﻿// створити метод: return_type name(parameters) { code }
+// void - нічого не повертає
+
+void PrintMessage(int count, string separator)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        Console.Write("Hello" + separator);
+    }
+    Console.WriteLine();
+}
+
+PrintMessage(10, "."); // 10
+
+// ... code
+
+PrintMessage(4, "-");
+PrintMessage(6, ", ");
+
+void ConvertUahToUsd(double uah)
+{
+    const double USD = 41.67;
+    Console.WriteLine($"You have: {uah / USD:0.##}$");
+}
+
+ConvertUahToUsd(1200);
+ConvertUahToUsd(50000);
+ConvertUahToUsd(5);
