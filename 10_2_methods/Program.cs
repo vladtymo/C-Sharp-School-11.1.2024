469bad5e8eb1b4dcc40b8142b541ac427bc51803
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-22 11:02:20 +0200

    Methods

---
+++ 10_2_methods/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.CursorVisible = false;
+
+void Shot(string direction, int startX, int startY)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    while (true)
+    {
+        Console.SetCursorPosition(startX, startY);
+        Console.Write("\u25cf");
+
+        Thread.Sleep(100);
+        
+        Console.SetCursorPosition(startX, startY);
+        Console.Write(" ");
+        switch (direction)
+        {
+            case "RIGHT": startX++; break;
+            case "LEFT": startX--; break;
+            case "UP": startY--; break;
+            case "DOWN": startY++; break;
+        }
+    }
+    Console.ResetColor();
+}
+
+Parallel.Invoke([
+    () => Shot("DOWN", 10, 0),
+    () => Shot("DOWN", 34, 10),
+    //() => Shot("DOWN", 0, 0),
+    //() => Shot("DOWN", 57, 30)
+]);
