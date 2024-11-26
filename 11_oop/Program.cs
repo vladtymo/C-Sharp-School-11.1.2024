ea3b6aa4a14e18438071f71275ae6e870409f88c
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-26 11:56:16 +0200

    Intro to OOP

---
+++ 11_oop/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Snake mainHero = new();
+
+        mainHero.x = 20;
+        mainHero.y = 9;
+        
+        mainHero.Print();
+        mainHero.TurnRight();
+        mainHero.Print();
+        mainHero.TurnDowm();
+        mainHero.Print();
+        mainHero.TurnDowm();
+        mainHero.Print();
+        mainHero.TurnDowm();
+        mainHero.Print();
+    }
+}
+
+class Snake
+{
+    // properties
+    public int x = 0;
+    public int y = 0; 
+    public int size = 1;
+    public ConsoleColor color = ConsoleColor.Blue;
+    public string symbol = "*";
+
+    // methods
+    public void Clear()
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(" ");
+    }
+    public void Print()
+    {
+        Console.ForegroundColor = color;
+        Console.SetCursorPosition(x, y);
+        Console.Write(symbol);
+        Console.ResetColor();
+    }
+
+    public void TurnRight()
+    {
+        x += 1;
+    }
+    public void TurnDowm()
+    {
+        if (y >= 10)
+            color = ConsoleColor.Yellow;
+        
+        y += 1;
+    }
+}
+
