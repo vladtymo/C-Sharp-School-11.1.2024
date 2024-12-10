af657d455c5e79e1bca37d24a55b9cf01813b722
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-12-10 11:41:59 +0200

    Files example

---
+++ 12_files/Program.cs
@@ -0,0 +1,25 @@
+﻿// ---- створюємо файл
+var file = File.CreateText("test.txt");
+
+// ----- додаємо текст до файлу
+file.WriteLine("Hello file!");
+file.WriteLine($"Today: {DateTime.Now}");
+file.WriteLine($"------------------------");
+
+// ----- закриваємо файл, щоб можна було далі з ним працювати
+file.Close();
+
+// ----- копіюємо файл в ту ж саму папку
+File.Copy("test.txt", "copy.txt", true);
+// ----- копіюємо файл на робочий стіл
+File.Copy("test.txt", $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}/copy.txt", true);
+
+// ----- читаємо текст з файлу
+string text = File.ReadAllText("test.txt");
+Console.WriteLine(text);
+
+// ----- видаляємо файл
+File.Delete("test.txt");
+
+// ----- створити файл під назвою користувача
+File.CreateText($"{Console.ReadLine()}.txt");
