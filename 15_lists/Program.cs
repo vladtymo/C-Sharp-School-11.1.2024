d0b15d6a0f41a83e4a50dbe5a00f8d229654fb38
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-13 12:44:50 +0200

    Colors example

---
+++ 15_lists/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+List<string> colors = new() {"green", "blue", "red", "yellow"};
+
+colors.Add("white");
+colors.Insert(1, "black");
+
+//foreach (var i in colors) Console.Write(i + ", ");
+Console.WriteLine("Inserted: " + string.Join(", ", colors)); 
+
+colors.RemoveAt(2);
+colors.Remove("yellow");
+
+Console.WriteLine("Removed: " + string.Join(", ", colors)); 
+
+Console.Write("Enter color to find: ");
+string color = Console.ReadLine();
+
+if (colors.Contains(color))
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Found color!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Color not found!");
+}
+Console.ResetColor();
+
+colors.Sort();
+Console.WriteLine("Sorted: " + string.Join(", ", colors));
+
+colors.Reverse();
+Console.WriteLine("Reversed: " + string.Join(", ", colors));
+
+Console.WriteLine("Colors count: " + colors.Count);
