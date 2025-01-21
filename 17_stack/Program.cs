5f3d6efc5d01d8aa4dc468563aafd915958409e8
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-21 11:51:54 +0200

    Stack example

---
+++ 17_stack/Program.cs
@@ -0,0 +1,22 @@
+﻿Stack<string> students = new Stack<string>();
+
+students.Push("Ivan");
+students.Push("Nazar");
+students.Push("Sofia");
+students.Push("Vika");
+students.Push("Vlad");
+
+// повернути та видалити верхній елемент
+Console.WriteLine("Top item: " + students.Pop());
+
+// повернути верхній елемент
+Console.WriteLine("Top item: " + students.Peek());
+
+Console.WriteLine("Students: " + students.Count);
+
+Console.WriteLine("Vlad: " + students.Contains("Vlad"));
+
+while (students.Count > 0)
+{
+    Console.WriteLine("Item: " + students.Pop());
+}
